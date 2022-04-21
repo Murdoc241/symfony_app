c39218ae057fa7e5d9ee75063938d240a3ed667d
Murdoc241 <ismaeltech241@gmail.com>
2022-04-21 19:40:59 +0200

    Ajout du formulaire d'inscription

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,4 +15,12 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+    #[Route('/inscription', name: 'app_inscription')]
+    public function inscription(): Response
+    {
+        return $this->render('inscription.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
 }
